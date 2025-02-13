166dc35f48b17ba93edd79e75587449823f05abd
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2025-02-12 20:08:25 -0500

    added proxy server and updated submodules

---
+++ proxy-server/index.js
@@ -0,0 +1,13 @@
+let host = process.env.HOST || "localhost";
+let port = process.env.PORT || 8080;
+
+let cors_proxy = require("cors-anywhere");
+cors_proxy
+  .createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ["origin", "x-requested-with"],
+    removeHeaders: ["cookie", "cookie2"],
+  })
+  .listen(port, host, function () {
+    console.log("Running CORS Anywhere on " + host + ":" + port);
+  });
