c07ee4a9455f8d318d00c1348ab8efb002462a53
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2025-03-09 19:09:30 -0400

    updated prisma schema

--- db/main.js
+++ db/main.js
@@ -2,3 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+await prisma.$transaction([
+  prisma.catalog.deleteMany({}),
+  prisma.course.deleteMany({}),
+  prisma.department.deleteMany({}),
+  prisma.major.deleteMany({}),
+  prisma.majorRequirements.deleteMany({}),
+  prisma.minor.deleteMany({}),
+  prisma.minorRequirements.deleteMany({}),
+  prisma.plan.deleteMany({}),
+  prisma.prerequisite.deleteMany({}),
+  prisma.user.deleteMany({}),
+]);
