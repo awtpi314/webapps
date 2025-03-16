55a132f2b74103e1e2cf07f406cf4d5e7a935ba6
Alexander Taylor <52684858+awtpi314@users.noreply.github.com>
2025-03-16 18:52:40 -0400

    update submodule and prisma

--- db/main.js
+++ db/main.js
@@ -1,16 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { hash, compare } from "bcrypt";
+import { createHash } from "crypto";
 
-const prisma = new PrismaClient();
-
-await prisma.$transaction([
-  prisma.catalog.deleteMany({}),
-  prisma.course.deleteMany({}),
-  prisma.department.deleteMany({}),
-  prisma.major.deleteMany({}),
-  prisma.majorRequirements.deleteMany({}),
-  prisma.minor.deleteMany({}),
-  prisma.minorRequirements.deleteMany({}),
-  prisma.plan.deleteMany({}),
-  prisma.prerequisite.deleteMany({}),
-  prisma.user.deleteMany({}),
-]);
+const password = "password";
+const sha256Hash = createHash("sha256").update(password).digest("hex");
+const bcryptHash =
+  "$2y$10$pvEeml/VHe1qPeioSvO5N.GwsVvoRdijdFMuKGTmUR6t3i3e53FC6";
+const genBcryptHash = await hash(sha256Hash, 10);
+console.log("SHA256 Hash:", sha256Hash);
+console.log("Bcrypt Hash:", bcryptHash);
+console.log("Generated Bcrypt Hash:", genBcryptHash);
+console.log(
+  "Are prepopulated hashes equal?",
+  await compare(sha256Hash, bcryptHash)
+);
+console.log(
+  "Are generated hashes equal?",
+  await compare(sha256Hash, genBcryptHash)
+);
